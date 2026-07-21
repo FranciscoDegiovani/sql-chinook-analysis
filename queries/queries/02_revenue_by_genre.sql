0e6f73e35208a17a5a61c2d16b7614716c777862
FranciscoDegiovani <francisco.degiovani@outlook.com>
2026-07-21 17:37:33 -0300

    Create SQL query for revenue by genre
    Add SQL query to retrieve top 5 music genres by revenue.

---
+++ queries/queries/02_revenue_by_genre.sql
@@ -0,0 +1,11 @@
+-- Question: Which music genre generates the most revenue?
+
+SELECT
+    g.Name AS Genre,
+    ROUND(SUM(il.UnitPrice * il.Quantity), 2) AS TotalRevenue
+FROM InvoiceLine il
+JOIN Track t ON il.TrackId = t.TrackId
+JOIN Genre g ON t.GenreId = g.GenreId
+GROUP BY g.Name
+ORDER BY TotalRevenue DESC
+LIMIT 5;
